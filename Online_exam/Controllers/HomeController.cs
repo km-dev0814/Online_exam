91b891d52ac92ce9d61e423a7addcdbe992c2bc4
km-dev0814 <km.dev0814@gmail.com>
2016-09-22 11:59:22 -0500

    initial commit

---
+++ Online_exam/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_exam.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

b6fd9d51c26566e74aa672e135fd94d9d09b9525
km-dev0814 <km.dev0814@gmail.com>
2016-09-22 14:03:34 -0500

    changed home controller and added layout and viewstart

--- Online_exam/Controllers/HomeController.cs
+++ Online_exam/Controllers/HomeController.cs
@@ -9,7 +9,31 @@ namespace Online_exam.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        // GET: Tab2
+        public ActionResult Tab2()
+        {
+            return View();
+        }
+
+        // GET: Tab3
+        public ActionResult Tab3()
+        {
+            return View();
+        }
+
+        // GET: Tab4
+        public ActionResult Tab4()
+        {
+            return View();
+        }
+
+        // GET: Contact Us
+        public ActionResult Contact_Us()
         {
             return View();
         }
